81c89874a9e440f0d2a053c2d7026bcfd780c02a
AbdalrahmanAbotaleb <abdalrahmanabotaleb362@gmail.com>
2023-09-25 16:57:03 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookapp/Features/Splash/presentation/views/SplashView.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+void main() {
+  runApp( BooklyApp());
+}
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+return GetMaterialApp(
+  home:  SplashView(),
+  
+);
+    
+  }
+}
